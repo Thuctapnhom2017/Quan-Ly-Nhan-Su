00bf6893ba24e1fb3fe56f47aa89b255d3dc7361
dungtutb <phamdunghc1@gmail.com>
2017-09-22 13:44:43 +0700

    code

---
+++ QuanLyNhanSu/QuanLyNhanSu/FormMain.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace QuanLyNhanSu
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.barQuanLyNhanVien = new DevExpress.XtraBars.BarButtonItem();
+            this.barQuaTrinhCongTac = new DevExpress.XtraBars.BarButtonItem();
+            this.barHopDongLaoDong = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem4 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem5 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem6 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem7 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem8 = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.panelMain = new DevExpress.XtraEditors.PanelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbon
+            // 
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.ExpandCollapseItem,
+            this.barQuanLyNhanVien,
+            this.barQuaTrinhCongTac,
+            this.barHopDongLaoDong,
+            this.barButtonItem4,
+            this.barButtonItem5,
+            this.barButtonItem6,
+            this.barButtonItem7,
+            this.barButtonItem8});
+            this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.MaxItemId = 10;
+            this.ribbon.Name = "ribbon";
+            this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbon.Size = new System.Drawing.Size(1236, 143);
+            this.ribbon.StatusBar = this.ribbonStatusBar;
+            // 
+            // barQuanLyNhanVien
+            // 
+            this.barQuanLyNhanVien.Caption = "Personnels";
+            this.barQuanLyNhanVien.Id = 2;
+            this.barQuanLyNhanVien.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barQuanLyNhanVien.ImageOptions.Image")));
+            this.barQuanLyNhanVien.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barQuanLyNhanVien.ImageOptions.LargeImage")));
+            this.barQuanLyNhanVien.Name = "barQuanLyNhanVien";
+            this.barQuanLyNhanVien.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barQuanLyNhanVien.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barQuanLyNhanVien_ItemClick_1);
+            // 
+            // barQuaTrinhCongTac
+            // 
+            this.barQuaTrinhCongTac.Caption = "Working";
+            this.barQuaTrinhCongTac.Id = 3;
+            this.barQuaTrinhCongTac.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barQuaTrinhCongTac.ImageOptions.Image")));
+            this.barQuaTrinhCongTac.Name = "barQuaTrinhCongTac";
+            this.barQuaTrinhCongTac.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barQuaTrinhCongTac.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barQuaTrinhCongTac_ItemClick_1);
+            // 
+            // barHopDongLaoDong
+            // 
+            this.barHopDongLaoDong.Caption = "Contract";
+            this.barHopDongLaoDong.Id = 4;
+            this.barHopDongLaoDong.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barHopDongLaoDong.ImageOptions.Image")));
+            this.barHopDongLaoDong.Name = "barHopDongLaoDong";
+            this.barHopDongLaoDong.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barHopDongLaoDong.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barHopDongLaoDong_ItemClick_1);
+            // 
+            // barButtonItem4
+            // 
+            this.barButtonItem4.Caption = "Discipline Reward";
+            this.barButtonItem4.Id = 5;
+            this.barButtonItem4.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem4.ImageOptions.Image")));
+            this.barButtonItem4.Name = "barButtonItem4";
+            this.barButtonItem4.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barButtonItem4.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem4_ItemClick);
+            // 
+            // barButtonItem5
+            // 
+            this.barButtonItem5.Caption = "User Management";
+            this.barButtonItem5.Id = 6;
+            this.barButtonItem5.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem5.ImageOptions.Image")));
+            this.barButtonItem5.Name = "barButtonItem5";
+            this.barButtonItem5.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barButtonItem5.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem5_ItemClick);
+            // 
+            // barButtonItem6
+            // 
+            this.barButtonItem6.Caption = "Follow department";
+            this.barButtonItem6.Id = 7;
+            this.barButtonItem6.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem6.ImageOptions.Image")));
+            this.barButtonItem6.Name = "barButtonItem6";
+            this.barButtonItem6.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barButtonItem6.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem6_ItemClick);
+            // 
+            // barButtonItem7
+            // 
+            this.barButtonItem7.Caption = "Position";
+            this.barButtonItem7.Id = 8;
+            this.barButtonItem7.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem7.ImageOptions.Image")));
+            this.barButtonItem7.Name = "barButtonItem7";
+            this.barButtonItem7.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barButtonItem7.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem7_ItemClick);
+            // 
+            // barButtonItem8
+            // 
+            this.barButtonItem8.Caption = "Department";
+            this.barButtonItem8.Id = 9;
+            this.barButtonItem8.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem8.ImageOptions.Image")));
+            this.barButtonItem8.Name = "barButtonItem8";
+            this.barButtonItem8.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonItemStyles.Large;
+            this.barButtonItem8.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem8_ItemClick);
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup2,
+            this.ribbonPageGroup3});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "Personnel Management";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.barQuanLyNhanVien);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barQuaTrinhCongTac);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barHopDongLaoDong);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem4);
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem5);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "Personnel Manangement";
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem6);
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "Count";
+            // 
+            // ribbonPageGroup3
+            // 
+            this.ribbonPageGroup3.ItemLinks.Add(this.barButtonItem7);
+            this.ribbonPageGroup3.ItemLinks.Add(this.barButtonItem8);
+            this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+            this.ribbonPageGroup3.Text = "Category";
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 680);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.ribbon;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(1236, 31);
+            // 
+            // panelMain
+            // 
+            this.panelMain.AutoSize = true;
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 143);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(1236, 537);
+            this.panelMain.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 711);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbon);
+            this.Name = "FormMain";
+            this.Ribbon = this.ribbon;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Personnel Management";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraBars.BarButtonItem barQuanLyNhanVien;
+        private DevExpress.XtraBars.BarButtonItem barQuaTrinhCongTac;
+        private DevExpress.XtraBars.BarButtonItem barHopDongLaoDong;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem4;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem5;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem6;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem7;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem8;
+        private DevExpress.XtraEditors.PanelControl panelMain;
+    }
+}
---
+++ QuanLyNhanSu/QuanLyNhanSu/FormMain.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using QuanLyNhanSu.DAO;
+
+namespace QuanLyNhanSu
+{
+    public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        #region sự kiện mở các form
+        private void barDanhMucChucVu_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmQuanLyChucVu quanlychucvu = new FrmQuanLyChucVu();
+            quanlychucvu.TopLevel = false;
+            quanlychucvu.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(quanlychucvu);
+            quanlychucvu.Show();
+        }
+
+        private void barDanhMucPhongBan_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmQuanLyPhongBan frmtg = new FrmQuanLyPhongBan();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barQuanLyNhanVien_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmQuanLyNhanVien frmtg = new FrmQuanLyNhanVien();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barHopDongLaoDong_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmHopDongLaoDong frmtg = new FrmHopDongLaoDong();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barQuaTrinhCongTac_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmQuaTrinhCongTac frmtg = new FrmQuaTrinhCongTac();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barThongTinKhenThuongKiLuat_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmKhenThuongKiLuat frmtg = new FrmKhenThuongKiLuat();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barThongKeTheoNhanVien_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmThongKeNhanVienTheoPhongBan frmtg = new FrmThongKeNhanVienTheoPhongBan();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barUser_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            FrmUser frmtg = new FrmUser();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        #endregion
+
+        private void barQuanLyNhanVien_ItemClick_1(object sender, ItemClickEventArgs e)
+        {
+            FrmQuanLyNhanVien frmtg = new FrmQuanLyNhanVien();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barQuaTrinhCongTac_ItemClick_1(object sender, ItemClickEventArgs e)
+        {
+            FrmQuaTrinhCongTac frmtg = new FrmQuaTrinhCongTac();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barHopDongLaoDong_ItemClick_1(object sender, ItemClickEventArgs e)
+        {
+            FrmHopDongLaoDong frmtg = new FrmHopDongLaoDong();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barButtonItem4_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            FrmKhenThuongKiLuat frmtg = new FrmKhenThuongKiLuat();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barButtonItem5_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            FrmUser frmtg = new FrmUser();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barButtonItem6_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            FrmThongKeNhanVienTheoPhongBan frmtg = new FrmThongKeNhanVienTheoPhongBan();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+
+        private void barButtonItem7_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            FrmQuanLyChucVu quanlychucvu = new FrmQuanLyChucVu();
+            quanlychucvu.TopLevel = false;
+            quanlychucvu.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(quanlychucvu);
+            quanlychucvu.Show();
+        }
+
+        private void barButtonItem8_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            FrmQuanLyPhongBan frmtg = new FrmQuanLyPhongBan();
+            frmtg.TopLevel = false;
+            frmtg.Dock = DockStyle.Fill;
+            panelMain.Controls.Clear();
+            panelMain.Controls.Add(frmtg);
+            frmtg.Show();
+        }
+    }
+}
