51f5ecdafeb3a380d0477b60b3d4ea183b2b9603
mihn-hieu <hieusay96@gmail.com>
2017-09-24 08:07:40 +0700

    viet xong ham nhe

---
+++ QuanLyNhanSu/QuanLyNhanSu/DAO/ThongKeDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.DAO
+{
+    public class ThongKeDAO
+    {
+        
+        public static DataTable ThongKeNVTheoPhongBan(int idpb)
+        {
+            string query = "SELECT N.HOTEN, N.GIOITINH, N.CHUCVUID, N.PHONGBANID, C.TEN AS TENCV, P.TEN AS TENPB, G.TEN AS TENGIOITINH  FROM NHANVIEN N, GIOITINH G, CHUCVU C, PHONGBAN P WHERE N.PHONGBANID = " + idpb + " AND N.CHUCVUID = C.CHUCVUID AND N.GIOITINH = G.GIOITINHID AND N.PHONGBANID = P.PHONGBANID";
+
+            return SqlServerHelper.ExecuteQuery(query);
+        }
+        public static DataTable ThongKeNVTheoChucVu(int idcv)
+        {
+            string query = "SELECT N.HOTEN, N.GIOITINH, N.CHUCVUID, N.PHONGBANID, C.TEN AS TENCV, P.TEN AS TENPB, G.TEN AS TENGIOITINH  FROM NHANVIEN N, GIOITINH G, CHUCVU C, PHONGBAN P WHERE N.CHUCVUID = " + idcv + " AND N.CHUCVUID = C.CHUCVUID AND N.GIOITINH = G.GIOITINHID AND N.PHONGBANID = P.PHONGBANID";
+
+            return SqlServerHelper.ExecuteQuery(query);
+        }
+    }
+}
