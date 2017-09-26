bbeac50ad9f9d9527faa646b7afdc00c3a57b55f
bounsy <32055547+bounsy@users.noreply.github.com>
2017-09-26 14:50:38 +0800

    e dung

---
+++ QuanLyNhanSu/QuanLyNhanSu/FormLogin/FrmLogin.cs
@@ -0,0 +1,43 @@
+﻿using QuanLyNhanSu;
+using QuanLyNhanSu.DAO;
+using QuanLyNhanSu.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string pass = textBox2.Text;
+            if (AccountDAO.Login(username, pass))
+            {
+                AccountDTO acc = AccountDAO.GetAccount(username);
+                string type = acc.Type.ToString();
+
+                FormMain frm = new FormMain();
+                frm.Show();
+
+            }
+        }
+    }
+}
